fe854dae08f4b0544c78fe0d279ea845d62289d3
salman <mac@macs-MacBook-Pro.local>
2022-07-27 14:03:35 +0500

    integraetd todo count api

--- src/components/Todo/TotalCount.js
+++ src/components/Todo/TotalCount.js
@@ -1,27 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
-import { listTodoCounts } from "../../graphql/queries";
 
 const TotalCount = (props) => {
-  useEffect(() => {
-    fetchTodoCount();
-  }, []);
-
-  async function fetchTodoCount() {
-    try {
-      const result = await API.graphql({
-        query: listTodoCounts,
-
-        authMode: "AMAZON_COGNITO_USER_POOLS",
-      });
-      //   API.graphql(graphqlOperation(listTodoCounts));
-      console.log(result);
-    } catch (err) {
-      console.log("error fetching todos", err);
-    }
-  }
-
-  return <></>;
+  // console.log();
+  return (
+    <h2>
+      Total Todos:
+      {props.totalCount.items && props.totalCount.items[0].totalCount}
+    </h2>
+  );
 };
 
 export default TotalCount;
